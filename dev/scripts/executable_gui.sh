5c6f4aef2b2955d3b5303ffeac4bd880554dbbb1
David Gonzalez Martin <davidgm94.work@protonmail.com>
2024-05-04 00:23:56 -0600

    Add vulkan-radeon and glfw

--- dev/scripts/executable_gui.sh
+++ dev/scripts/executable_gui.sh
@@ -2,4 +2,4 @@
 set -ex
 
 # This is the GUI-needed apps that are installed quickly
-yay -Sy --noconfirm xorg-xwininfo qbittorrent telegram-desktop signal-desktop vulkan-devel xorg-xkill gparted mpv vlc smplayer pavucontrol pulsemixer playerctl libreoffice discord parsec-bin google-chrome
+yay -Sy --noconfirm xorg-xwininfo qbittorrent telegram-desktop signal-desktop vulkan-devel xorg-xkill gparted mpv vlc smplayer pavucontrol pulsemixer playerctl libreoffice discord parsec-bin google-chrome glfw vulkan-radeon
