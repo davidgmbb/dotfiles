0b514f1ccaa4b12656e10ce17e736f84ba3fb7d8
David Gonzalez Martin <davidgm94.work@protonmail.com>
2024-05-05 14:48:43 -0600

    Add git-lfs

--- dev/scripts/executable_base.sh
+++ dev/scripts/executable_base.sh
@@ -2,7 +2,7 @@
 set -ex
 # Update database and current packages and install desired packages
 sudo pacman -Syu --needed --noconfirm\
-    kitty ripgrep bat neovim xclip zip tree fd zsh gdb qemu-full rustup rust-analyzer nodejs tokei xorriso llvm cmake lld clang ninja nasm ctags mtools perf\
+    kitty git-lfs ripgrep bat neovim xclip zip tree fd zsh gdb qemu-full rustup rust-analyzer nodejs tokei xorriso llvm cmake lld clang ninja nasm ctags mtools perf\
     ttf-fira-mono ttf-fira-code ttf-fira-sans ttf-dejavu ttf-liberation noto-fonts\
 
 
