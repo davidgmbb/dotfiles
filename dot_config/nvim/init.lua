f07def707a38976906ec79d5625c26983748d13a
David Gonzalez Martin <davidgm94.work@protonmail.com>
2025-01-01 07:55:05 -0600

    Lighten neovim

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -50,9 +50,9 @@ vim.g.vimtex_compiler_method = 'latexrun'
 require("lazy").setup({
     { 'ellisonleao/gruvbox.nvim' },
 
-    { 'neovim/nvim-lspconfig' },
+    -- { 'neovim/nvim-lspconfig' },
     { 'hrsh7th/nvim-cmp'},
-    { 'hrsh7th/cmp-nvim-lsp'},
+    -- { 'hrsh7th/cmp-nvim-lsp'},
     { 'hrsh7th/cmp-vsnip'},
     { 'hrsh7th/cmp-path'},
     { 'hrsh7th/cmp-buffer'},
@@ -66,31 +66,31 @@ require("lazy").setup({
 
     { "lewis6991/gitsigns.nvim", config = function() require("gitsigns").setup() end, },
     { 'f-person/git-blame.nvim' },
-    -- { "folke/trouble.nvim", dependencies = { { "nvim-tree/nvim-web-devicons", lazy = false } }, config = function() require("trouble").setup() end },
+    { "folke/trouble.nvim", dependencies = { { "nvim-tree/nvim-web-devicons", lazy = false } }, config = function() require("trouble").setup() end },
     -- { "folke/lsp-colors.nvim" },
 })
 
 vim.o.background = "dark" -- or "light" for light mode
 vim.cmd([[colorscheme gruvbox]])
 
-local nvim_lsp = require'lspconfig'
+-- local nvim_lsp = require'lspconfig'
 
-local lsp_keymap_opts = {}
-vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, lsp_keymap_opts)
-vim.keymap.set('n', 'gD', function() vim.lsp.buf.implementation() end, lsp_keymap_opts)
-vim.keymap.set('n', '<c-k>', function() vim.lsp.buf.signature_help() end, lsp_keymap_opts)
-vim.keymap.set('n', '1gD', function() vim.lsp.buf.type_definition() end, lsp_keymap_opts)
-vim.keymap.set('n', 'gr', function() vim.lsp.buf.references() end, lsp_keymap_opts)
-vim.keymap.set('n', 'g0', function() vim.lsp.buf.document_symbol() end, lsp_keymap_opts)
-vim.keymap.set('n', 'gW', function() vim.lsp.buf.workspace_symbol() end, lsp_keymap_opts)
-vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, lsp_keymap_opts)
-vim.keymap.set('n', 'ge', function() vim.diagnostic.goto_next() end, lsp_keymap_opts)
-vim.keymap.set('n', 'ga', function() vim.lsp.buf.code_action() end, lsp_keymap_opts)
-vim.keymap.set('n', 'gh', ':ClangdSwitchSourceHeader<CR>', lsp_keymap_opts)
+-- local lsp_keymap_opts = {}
+-- vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, lsp_keymap_opts)
+-- vim.keymap.set('n', 'gD', function() vim.lsp.buf.implementation() end, lsp_keymap_opts)
+-- vim.keymap.set('n', '<c-k>', function() vim.lsp.buf.signature_help() end, lsp_keymap_opts)
+-- vim.keymap.set('n', '1gD', function() vim.lsp.buf.type_definition() end, lsp_keymap_opts)
+-- vim.keymap.set('n', 'gr', function() vim.lsp.buf.references() end, lsp_keymap_opts)
+-- vim.keymap.set('n', 'g0', function() vim.lsp.buf.document_symbol() end, lsp_keymap_opts)
+-- vim.keymap.set('n', 'gW', function() vim.lsp.buf.workspace_symbol() end, lsp_keymap_opts)
+-- vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, lsp_keymap_opts)
+-- vim.keymap.set('n', 'ge', function() vim.diagnostic.goto_next() end, lsp_keymap_opts)
+-- vim.keymap.set('n', 'ga', function() vim.lsp.buf.code_action() end, lsp_keymap_opts)
+-- vim.keymap.set('n', 'gh', ':ClangdSwitchSourceHeader<CR>', lsp_keymap_opts)
 
-nvim_lsp.zls.setup{}
-nvim_lsp.clangd.setup{}
-nvim_lsp.rust_analyzer.setup{}
+-- nvim_lsp.zls.setup{}
+-- nvim_lsp.clangd.setup{}
+-- nvim_lsp.rust_analyzer.setup{}
 
 local cmp = require'cmp'
 cmp.setup({
@@ -117,7 +117,7 @@ cmp.setup({
 
   -- Installed sources
   sources = {
-    { name = 'nvim_lsp' },
+    -- { name = 'nvim_lsp' },
     { name = 'vsnip' },
     { name = 'path' },
     { name = 'buffer' },
@@ -126,7 +126,44 @@ cmp.setup({
 
 local telescope_builtin = require('telescope.builtin')
 
-vim.keymap.set('n', '<leader>d', telescope_builtin.find_files, {})
+vim.keymap.set('n', '<leader>q', telescope_builtin.find_files, {})
 vim.keymap.set('n', '<leader>f', telescope_builtin.git_files, {})
 vim.keymap.set('n', '<leader>g', telescope_builtin.live_grep, {})
 vim.keymap.set('n', '<leader>r', telescope_builtin.resume, {})
+
+local function compile_common(compile_command)
+  vim.cmd(compile_command) -- Run :make
+  local exit_code = vim.v.shell_error
+  local qf_exists = not vim.tbl_isempty(vim.fn.getqflist({ items = 1 }).items)
+  if qf_exists then
+      vim.cmd("Trouble qflist toggle") -- Open the quickfix list if there are errors
+  end
+  if exit_code ~= 0 then
+      return false
+  else
+      return true
+  end
+end
+
+local function just_compile()
+    compile_common("silent! make")
+end
+
+local function compile_and_test()
+    compile_common("silent! make test")
+end
+
+local function compile_and_debug()
+    compile_common("silent! make debug")
+end
+
+local os_string = vim.loop.os_uname().sysname
+if os_string == "Windows" then
+    vim.opt.makeprg = "build.bat"
+else
+    vim.opt.makeprg = "./build.c"
+end
+
+vim.keymap.set('n', '<leader>c', just_compile, {})
+vim.keymap.set('n', '<leader>t', compile_and_test, {})
+vim.keymap.set('n', '<leader>d', compile_and_debug, {})
