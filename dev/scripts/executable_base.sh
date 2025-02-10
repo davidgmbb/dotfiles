13937cf9c63891d56606fb62308403867605333e
David Gonzalez Martin <davidgm94.work@protonmail.com>
2025-02-10 12:00:45 -0600

    Install mold, strace, htop

--- dev/scripts/executable_base.sh
+++ dev/scripts/executable_base.sh
@@ -3,7 +3,7 @@ set -ex
 # Update database and current packages and install desired packages
 sudo pacman -Syu --needed --noconfirm\
     kitty git-lfs ripgrep bat neovim xclip zip tree fd zsh gdb qemu-full rustup rust-analyzer nodejs tokei xorriso llvm cmake lld clang ninja nasm ctags mtools perf\
-    ttf-fira-mono ttf-fira-code ttf-fira-sans ttf-dejavu ttf-liberation noto-fonts\
+    ttf-fira-mono ttf-fira-code ttf-fira-sans ttf-dejavu ttf-liberation noto-fonts strace mold htop\
 
 
 # Make ZSH the default shell
